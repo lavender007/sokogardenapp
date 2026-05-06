0a31a8172acc6cd58725f93cd947d8a4061dff64
Samantha Mayaka <mayakasamantha6@gmail.com>
2026-05-06 16:08:39 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import{BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Getproducts from './components/Getproducts'
+import Addproducts from './components/Addproducts';
+import Signup from './components/Signup';
+import Signin from './components/Signin';
+import NotFound from './components/NotFound';
+
 
 function App() {
   return (
-    <div className="App">
+   <Router>
+     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Welcome to Sokogarden</h1>
       </header>
+      {/*Below are our different routes binding the components together with the rendered cmponents*/}
+      <Routes>
+        <Route path = '/'element={<Getproducts />}/>
+        <Route path = '/addproducts' element={<Addproducts />}/>
+        <Route path = '/signup' element={<Signup />}/>
+        <Route path = '/signin' element = {<Signin />}/>
+        <Route path = '/*' element = {<NotFound />} />
+        
+      </Routes>
     </div>
+   </Router>
   );
 }
 
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../css/NotFound.css'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Sorry Page Not Found</h1>
+      <h1>404</h1>
+      <a href ='/'> Back home</a>
+    </div>
+  )
+}
+
+export default NotFound;
